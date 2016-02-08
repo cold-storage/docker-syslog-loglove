6db7f75c26eefffab6d714e820a7188edfad3c48
John Stein <john@johnandkerri.com>
2016-02-08 00:58:07 -0500

    SIGTERM

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ var log = new(require('loglove'))().log(__filename.substring(process.cwd().lengt
 
 log.info('we are logging out something here.');
 
+// I am not sure why but outside of docker your node app will exit immediately
+// on SIGTERM even if you don't have a SIGTERM handler. But when you run your
+// app in a docker it does not immediately quit on SIGTERM. It takes 10
+// seconds and then docker has to SIGKILL it.
+process.on('SIGTERM', function() {
+  console.log('SIGTERM');
+  process.exit(0);
+});
+
 setInterval(function() {
   log.info('we are logging out something here.');
 }, 5000);
